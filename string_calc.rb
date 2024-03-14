4b2f9ddf40b7557d1f8f71f722fb33f5c68aabbf
Reshma TP <reshmatpsasidharan@gmail.com>
2024-03-14 15:33:25 +0530

    Commit2: Handled the case of negative numbers and new line character

--- string_calc.rb
+++ string_calc.rb
@@ -2,13 +2,28 @@ def add(str)
     if str.empty?  
         return 0
     else
-        sum=0
+        str.gsub!("\n",",")
+        p str
         arr=str.split(",")
-        for i in arr
-            sum+=i.to_i   
+        p arr
+        flag=false
+        n_num=[]
+        for i in 0..arr.length-1  
+            num=arr[i].to_i
+            if num<0  
+                n_num<<num 
+                flag=true   
+            end
         end
-        return sum
+        if flag==true  
+            return "negative numbers not allowed, #{n_num}"
+        else
+            sum=0
+            for i in arr
+                sum+=i.to_i  
+            end
+            return sum
+        end
     end 
 end
-p add(" ")
-p add("1,45,67,32,34,34,12,2")
+p add("1,-4,-5")
